acc1fb0f90eb3c973ed68263b942ce09b8b27a8a
Rakshith <44756824+RajaRakshith@users.noreply.github.com>
2026-05-18 22:51:33 -0400

    leader + cp to copy whole file to clipboard:

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = " "
 map("n", "<leader>cd", vim.cmd.Ex)
 map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+map("n", "<leader>cp", ":%y+<CR>", { desc = "Copy All" })
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 
 
