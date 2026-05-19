cac44f5cd64eecb908deb5e3842d8306e0963ce5
Rakshith <44756824+RajaRakshith@users.noreply.github.com>
2026-05-19 19:35:03 -0400

    leader + c to copy, leader + p to paste in nvim

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,10 +1,12 @@
 local map = vim.keymap.set
 
 vim.g.mapleader = " "
-map("n", "<leader>cd", vim.cmd.Ex)
+map("n", "<leader>cd", vim.cmd.Ex, {desc = "File Browser"})
 map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 map("n", "<leader>cp", ":%y+<CR>", { desc = "Copy All" })
+map("n", "<leader>p", "\"+p", { desc = "Paste" })
+map("n", "<leader>c", "\"+y", { desc = "Copy" })
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 
 
